c33dc97b37f7dda0033425136ba607d2de11467f
melonybox <grandmadcow@gmail.com>
2020-04-12 22:35:08 -0400

    Python syntax learning

---
+++ code_challenges/day_1_edabit.py
@@ -0,0 +1,30 @@
+def is_same_num(num1, num2):
+	if num1 == num2:
+		return True
+	else:
+		return False
+
+# ---
+
+def find_perimeter(height, width):
+	return (height+width)*2
+
+# ---
+
+def add_ab(a,b):
+	return a+b
+
+# ---
+
+def concat_name(first_name, last_name):
+	newSentence = last_name + ", " + first_name
+	return newSentence
+
+#Strings are immutable in pythons
+
+# ---
+
+def frames(minutes, fps):
+	return minutes*(fps*60)
+
+# ---
